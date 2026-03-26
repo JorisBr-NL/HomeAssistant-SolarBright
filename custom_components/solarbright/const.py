d9e15b237a9850f2b974e5b5dd6011c8600b632d
JorisBr-NL <github@itsme.simplelogin.com>
2026-03-26 20:21:32 +0100

    Create const.py

---
+++ custom_components/solarbright/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "solarbright"
+DEFAULT_PORT = 80
+DEFAULT_SCAN_INTERVAL = 360  # seconds
+
+DATA_COORDINATORS = "coordinators"
+
+# Solar panel settings for forecast
+PANEL_AREA = 20  # m²
+PANEL_EFFICIENCY = 0.18
+PANEL_TILT = 30  # degrees
+PANEL_AZIMUTH = 180  # degrees, south-facing
