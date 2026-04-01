143bc81f7cb56ec6ff6a125813470651d29bd97a
Joris Bakker <joris@bakkeronline.org>
2026-04-01 16:08:35 +0200

    Update const.py

--- custom_components/solarbright/const.py
+++ custom_components/solarbright/const.py
@@ -1,2 +1,9 @@
 DOMAIN = "solar_inverter"
-DEFAULT_SCAN_INTERVAL = 120
+
+CONF_HOST = "host"
+CONF_SCAN_INTERVAL = "scan_interval"
+CONF_DAY_OFFSET = "day_offset"
+
+DEFAULT_SCAN_INTERVAL = 120  # seconds
+DEFAULT_DAY_OFFSET = 30      # minutes before/after sun
+NIGHT_SCAN_INTERVAL = 1800    # 30 min fallback at night
