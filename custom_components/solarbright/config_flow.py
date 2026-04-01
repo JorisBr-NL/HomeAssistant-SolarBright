b3103175287c421118aeba4ac6a37d2dd963e7cd
Joris Bakker <joris@bakkeronline.org>
2026-04-01 16:09:49 +0200

    Update config_flow.py

--- custom_components/solarbright/config_flow.py
+++ custom_components/solarbright/config_flow.py
@@ -1,20 +1,19 @@
 import voluptuous as vol
 from homeassistant import config_entries
-
-from .const import DOMAIN
-
+from .const import DOMAIN, CONF_HOST, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
 
 class SolarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(
-                title=f"Inverter ({user_input['host']})",
+                title=f"Inverter ({user_input[CONF_HOST]})",
                 data=user_input,
             )
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("host"): str,
+                vol.Required(CONF_HOST): str,
+                vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
             }),
         )
