9ab7d0e5b088f49e0223801b346715c74b24ce3a
Joris Bakker <joris@bakkeronline.org>
2026-04-01 09:28:36 +0200

    Update __init__.py

--- custom_components/solarbright/__init__.py
+++ custom_components/solarbright/__init__.py
@@ -1,16 +1,15 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from .const import DOMAIN
+
 from .coordinator import SolarBrightCoordinator
 
-PLATFORMS = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    session = async_get_clientsession(hass)
-    coordinator = SolarBrightCoordinator(hass, session, entry.data["ip"])
+    coordinator = SolarBrightCoordinator(hass, entry.data["host"])
     await coordinator.async_config_entry_first_refresh()
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    entry.runtime_data = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
