03d766878cf4480d5b9667386f130c4fcb0055dd
Joris Bakker <joris@bakkeronline.org>
2026-04-01 09:29:08 +0200

    Update config_flow.py

--- custom_components/solarbright/config_flow.py
+++ custom_components/solarbright/config_flow.py
@@ -5,19 +5,14 @@ from homeassistant import config_entries
 from .const import DOMAIN
 
 
-class SolarBrightConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
-
+class SolarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(
-                title=f"SolarBright ({user_input['ip']})",
-                data=user_input,
-            )
+            return self.async_create_entry(title="Solar Inverter", data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("ip"): str,
+                vol.Required("host"): str,
             }),
         )
