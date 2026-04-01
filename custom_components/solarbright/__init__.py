08d737e134148376652223d24e2f55f70d252115
Joris Bakker <joris@bakkeronline.org>
2026-04-01 14:54:15 +0200

    Update __init__.py

--- custom_components/solarbright/__init__.py
+++ custom_components/solarbright/__init__.py
@@ -1,11 +1,14 @@
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .coordinator import SolarBrightCoordinator
+from .coordinator import SolarInverterCoordinator
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    coordinator = SolarBrightCoordinator(hass, entry.data["host"])
+    coordinator = SolarInverterCoordinator(hass, entry.data["host"])
     await coordinator.async_config_entry_first_refresh()
 
     entry.runtime_data = coordinator
