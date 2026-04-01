d41e60d96ac1141ad0b131f6089c3fe05ec9b29e
Joris Bakker <joris@bakkeronline.org>
2026-04-01 21:18:42 +0200

    Update __init__.py

--- custom_components/solarbright/__init__.py
+++ custom_components/solarbright/__init__.py
@@ -1,18 +1,28 @@
-import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN, PLATFORMS
 from .coordinator import SolarInverterCoordinator
 
-_LOGGER = logging.getLogger(__name__)
+PLATFORMS = ["sensor"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    coordinator = SolarInverterCoordinator(hass, entry.data["host"])
+    """Set up SolarBright from a config entry."""
+
+    coordinator = SolarInverterCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
 
-    entry.runtime_data = coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
